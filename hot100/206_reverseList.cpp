32f55a9dd783672ffb3f629e7d4337edaf664b69
MrSu18 <3051605203@qq.com>
2026-03-11 11:37:36 +0800

    206.反转链表，根据五年前的QG练习写出

---
+++ hot100/206_reverseList.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution 
+{
+public:
+    ListNode* reverseList(ListNode* head) 
+    {
+        if (head==NULL || head->next==NULL)
+        {
+            return head;
+        }
+        
+        ListNode *pre=head, *cur=head->next,*next_node;
+        pre->next=NULL;
+        while (cur!=NULL)
+        {
+            next_node=cur->next;//先记录好下一个节点的位置不要丢失了
+            //开始翻转
+            cur->next=pre;
+            pre=cur;
+            cur=next_node;
+        }
+        return pre;
+    }
+};
+
+int main()
+{
+    vector<int> list = {1,2,3,4,5};
+    ListNode *common = new ListNode(list[0]);
+    ListNode *temp=common;
+    for (size_t i = 1; i < list.size(); i++)
+    {
+        temp->next=new ListNode(list[i]);
+        temp = temp->next;
+    }
+    Solution test;
+    test.reverseList(common);
+    return 0;
+}
