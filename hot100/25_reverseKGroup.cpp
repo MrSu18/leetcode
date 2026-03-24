a182de170528808b47247cef7df7bf624bf46543
MrSu18 <3051605203@qq.com>
2026-03-24 17:08:05 +0800

    25.翻转K个一组链表-不会写，没写出来

--- hot100/25_reverseKGroup.cpp
+++ hot100/25_reverseKGroup.cpp
@@ -33,3 +33,39 @@ public:
            
     }
 };
+
+
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        // 统计节点个数
+        int n = 0;
+        for (ListNode* cur = head; cur; cur = cur->next) {
+            n++;
+        }
+
+        ListNode dummy(0, head);
+        ListNode* p0 = &dummy;
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+
+        // k 个一组处理
+        for (; n >= k; n -= k) {
+            for (int i = 0; i < k; i++) { // 同 92 题
+                ListNode* nxt = cur->next;
+                cur->next = pre; // 每次循环只修改一个 next，方便大家理解
+                pre = cur;
+                cur = nxt;
+            }
+
+            // 见视频
+            ListNode* nxt = p0->next;
+            p0->next->next = cur;
+            p0->next = pre;
+            p0 = nxt;
+        }
+        return dummy.next;
+    }
+};
+
